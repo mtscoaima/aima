072ab8e4059e8de74986fea37c52d029aa2e47d1
yoongun97 <tesung5470@naver.com>
2025-06-16 15:47:27 +0900

    fix: Footer style 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,20 +9,21 @@ export default function Footer() {
     <footer className="footer footer--landing">
       <div className="footer-landing-inner">
         <div className="footer-landing-left">
-          <div className="footer-landing-logo-row">
-            <Image
-              src="/images/aima-logo-footer.svg"
-              alt="aima logo"
-              width={32}
-              height={32}
-              className="footer-landing-logo-img"
-            />
-            <span className="footer-landing-logo-text">aima</span>
-          </div>
+          <Image
+            src="/images/aima-logo-footer.svg"
+            alt="aima logo"
+            width={106}
+            height={27}
+            className="footer-landing-logo-img"
+          />
           <div className="footer-landing-links">
-            <Link href="#">서비스 이용약관</Link>
+            <Link href="#" className="footer-landing-terms">
+              서비스 이용약관
+            </Link>
             <span className="footer-landing-divider">|</span>
-            <Link href="#">개인정보처리방침</Link>
+            <Link href="#" className="footer-landing-privacy">
+              개인정보처리방침
+            </Link>
           </div>
           <div className="footer-landing-company-info">
             (주)엠티에스컴퍼니 | 주소: 서울특별시 강남구 삼성로 434, 8층(대치동,
@@ -37,16 +38,18 @@ export default function Footer() {
         <div className="footer-landing-right">
           <div className="footer-landing-customer">
             <div className="footer-landing-customer-title">고객센터</div>
-            <div>대표번호: 02-111-1111</div>
-            <div>이메일: aaaa@aima.com</div>
+            <div className="footer-landing-customer-contact">
+              <div>대표번호: 02-111-1111</div>
+              <div>이메일: aaaa@aima.com</div>
+            </div>
             <div>평일 10:00 ~ 17:00 (점심 12:00~13:00) 주말, 공휴일 제외</div>
           </div>
           <div className="footer-landing-certifications">
             <Image
               src="/images/cert.png"
               alt="인증마크1"
-              width={220}
-              height={38}
+              width={417}
+              height={97}
               className="footer-landing-cert-img"
             />
           </div>
